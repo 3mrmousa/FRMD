8e05b8f8863cd5dc463ab2d24a401e6f512dc40a
3mrmousa <amrmoda123@gmail.com>
2026-05-04 04:07:53 +0300

    comment all store

--- store/slices/cartSlice.ts
+++ store/slices/cartSlice.ts
@@ -1,55 +1,53 @@
-import { createSlice } from "@reduxjs/toolkit";
+// import { createSlice } from "@reduxjs/toolkit";
 
-type CartItem = {
-  id: string;
-  name: string;
-  price: number;
-  quantity: number;
-};
+// type CartItem = {
+//   id: string;
+//   name: string;
+//   price: number;
+//   quantity: number;
+// };
 
-type CartState = {
-  items: CartItem[];
-  total: number;
-};
+// type CartState = {
+//   items: CartItem[];
+//   total: number;
+// };
 
-const initialState: CartState = {
-  items: [],
-  total: 0,
-};
+// const initialState: CartState = {
+//   items: [],
+//   total: 0,
+// };
 
-const cartSlice = createSlice({
-  name: "cart",
-  initialState,
-  reducers: {
-    addToCart: (state, action) => {
-      const existingItem = state.items.find(
-        (item) => item.id === action.payload.id,
-      );
-      if (existingItem) {
-        existingItem.quantity += 1;
-        state.total += existingItem.price;
-      } else {
-        state.items.push(action.payload);
-        state.total += action.payload.price;
-      }
-    },
-    removeFromCart: (state, action) => {
-      const itemToRemove = state.items.find(
-        (item) => item.id === action.payload.id,
-      );
+// const cartSlice = createSlice({
+//   name: "cart",
+//   initialState,
+//   reducers: {
+//     addToCart: (state, action) => {
+//       const existingItem = state.items.find(
+//         (item) => item.id === action.payload.id,
+//       );
+//       if (existingItem) {
+//         existingItem.quantity += 1;
+//         state.total += existingItem.price;
+//       } else {
+//         state.items.push(action.payload);
+//         state.total += action.payload.price;
+//       }
+//     },
+//     removeFromCart: (state, action) => {
+//       const itemToRemove = state.items.find(
+//         (item) => item.id === action.payload.id,
+//       );
 
-      if (!itemToRemove) return;
+//       if (!itemToRemove) return;
 
-      item.quantity -= 1;
-      state.total -= itemToRemove.price;
-      if (itemToRemove.quantity <= 0) {
-        state.items = state.items.filter(
-          (item) => item.id !== action.payload.id,
-        );
-      }
-    },
-  },
-});
+//       item.quantity -= 1;
+//       state.total -= itemToRemove.price;
+//       if (itemToRemove.quantity <= 0) {
+//         state.items = state.items.filter((item) => item.id !== action.payload.id);
+//       }
+//     },
+//   },
+// });
 
-export const { addToCart, removeFromCart } = cartSlice.actions;
-export default cartSlice.reducer;
+// export const { addToCart, removeFromCart } = cartSlice.actions;
+// export default cartSlice.reducer;
--- store/store.ts
+++ store/store.ts
@@ -1,10 +1,10 @@
-import { configureStore } from "@reduxjs/toolkit";
-import userReducer from "./slices/userSlice";
-import cartReducer from "./slices/cartSlice";
+// import { configureStore } from "@reduxjs/toolkit";
+// import userReducer from "./slices/userSlice";
+// import cartReducer from "./slices/cartSlice";
 
-export const store = configureStore({
-    reducer: {
-        user: userReducer,
-        cart: cartReducer,
-    }
-})
+// export const store = configureStore({
+//     reducer: {
+//         user: userReducer,
+//         cart: cartReducer,
+//     }
+// })
